b018ef7912105448b0a525bd608cda03c20ba614
aleksandrovvr.22 <vladandroid3@gmail.com>
2021-03-16 15:10:19 +0300

    yell

--- src/main/java/problem/Problem.java
+++ src/main/java/problem/Problem.java
@@ -3,6 +3,7 @@ package problem;
 import javax.media.opengl.GL2;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -14,8 +15,10 @@ public class Problem {
      */
     public static final String PROBLEM_TEXT = "ПОСТАНОВКА ЗАДАЧИ:\n" +
             "На плоскости задано множество точек.\n" +
-            "Найти такие две окружности, что их центры находятся в точках заданного множества, \n" +
-            "Внутри каждой из этих окружностей находится хотя бы половина из всех точек заданного множества. \n" +
+            "Найти такие две окружности,\n" +
+            "Что их центры находятся в точках заданного множества, \n" +
+            "Внутри каждой из этих окружностей \n" +
+            "Находится хотя бы половина из всех точек заданного множества. \n" +
             "Меньший из двух радиусов минимален.";
 
     /**
@@ -33,6 +36,7 @@ public class Problem {
      */
     private ArrayList<Point> points;
     private Circle circle;
+    private Circle Rcircle;
 
     /**
      * Конструктор класса задачи
@@ -61,9 +65,7 @@ public class Problem {
             p.solve = false;
         }
         Circle circle1 = new Circle();
-        double minR = 4;
-        double cenx = 0;
-        double ceny = 0;
+        circle = new Circle(0, 0, 4);
         int k = 0;
         for (Point p : points){
             for (Point p2 : points){
@@ -75,10 +77,10 @@ public class Problem {
                         k++;
                     }
                     if (((k == points.size() / 2) && (points.size() % 2 == 0)) || ((k == (points.size() / 2) + 1) && (points.size() % 2 == 1))){
-                        if (minR - circle1.r >= 0.00001){
-                            minR = circle1.r;
-                            cenx = circle1.x;
-                            ceny = circle1.y;
+                        if (circle.r > circle1.r){
+                            circle.r = circle1.r;
+                            circle.x = circle1.x;
+                            circle.y = circle1.y;
                         }
                         break;
                     }
@@ -86,7 +88,32 @@ public class Problem {
                 k = 0;
             }
         }
-        this.circle = new Circle(cenx, ceny, minR);
+        k = 0;
+        Rcircle = new Circle(0, 0, 4);
+        for (Point p3 : points) {
+            for (Point p : points) {
+                circle1.SetR(p);
+                circle1.x = p3.x;
+                circle1.y = p3.y;
+                for (Point p2 : points) {
+                    if ((circle1.x == circle.x) && (circle1.y == circle.y)){
+                        break;
+                    }
+                    if (Rcircle.inside(p2)) {
+                        k++;
+                    }
+                    if (((k == points.size() / 2) && (points.size() % 2 == 0)) || ((k == (points.size() / 2) + 1) && (points.size() % 2 == 1))) {
+                        if (Rcircle.r > circle1.r){
+                            Rcircle.r = circle1.r;
+                            Rcircle.x = circle1.x;
+                            Rcircle.y = circle1.y;
+                        }
+                        break;
+                    }
+                }
+                k = 0;
+            }
+        }
         for (Point p : points){
             if (circle.inside(p)){
                 p.solve = true;
@@ -148,6 +175,7 @@ public class Problem {
     public void clear() {
         points.clear();
         circle = null;
+        Rcircle = null;
     }
 
     /**
@@ -160,5 +188,8 @@ public class Problem {
             point.render(gl);
         }
         if (circle != null){circle.render(gl);}
+        if (Rcircle != null){
+            Rcircle.render(gl);
+        }
     }
 }
